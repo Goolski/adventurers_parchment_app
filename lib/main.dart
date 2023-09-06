19c5ac24747047569054b000d093cac64d60aabf
Goolski <andrzej.golski1@gmail.com>
2023-09-06 15:36:44 +0200

    Simple List displaying all of the Spells

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dnd_app/spells_data_source.dart';
+import 'package:flutter/material.dart';
 
 class MyHttpOverrides extends HttpOverrides {
   @override
@@ -13,6 +14,61 @@ class MyHttpOverrides extends HttpOverrides {
 
 void main() {
   HttpOverrides.global = MyHttpOverrides();
-  final spells = SpellsDataSource();
-  spells.getAllSpellNames();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: AllSpellsView(),
+    );
+  }
+}
+
+class AllSpellsView extends StatefulWidget {
+  const AllSpellsView({super.key});
+
+  @override
+  State<AllSpellsView> createState() => _AllSpellsViewState();
+}
+
+class _AllSpellsViewState extends State<AllSpellsView> {
+  late final Future<List<String>> spells;
+
+  @override
+  void initState() {
+    spells = SpellsDataSource().getAllSpellNames();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: FutureBuilder(
+          future: spells,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) => ListTile(
+                  onTap: () {},
+                  title: Text(
+                    snapshot.data![index],
+                  ),
+                ),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
 }
--- lib/spells_data_source.dart
+++ lib/spells_data_source.dart
@@ -5,7 +5,7 @@ const apiPath = 'https://www.dnd5eapi.co/api';
 class SpellsDataSource {
   final _dio = Dio();
 
-  Future<void> getAllSpellNames() async {
+  Future<List<String>> getAllSpellNames() async {
     final response = await _dio.get(
       apiPath + '/spells',
       options: Options(
@@ -17,6 +17,6 @@ class SpellsDataSource {
     List<dynamic> results = response.data["results"];
     List<String> names =
         results.map((result) => result["name"] as String).toList();
-    print(names);
+    return names;
   }
 }
