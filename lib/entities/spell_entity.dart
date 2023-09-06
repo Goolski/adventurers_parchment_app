f4354217a83a61a200e408e5eec39e70f75ccda9
Goolski <andrzej.golski1@gmail.com>
2023-09-06 16:24:42 +0200

    Created SpellEntity

---
+++ lib/entities/spell_entity.dart
@@ -0,0 +1,11 @@
+class SpellEntity {
+  final String index;
+  final String name;
+  final String url;
+
+  SpellEntity({
+    required this.index,
+    required this.name,
+    required this.url,
+  });
+}
