eb59404bf7ba6a72a2c0cb98117cc32ba3a6adc0
Goolski <andrzej.golski1@gmail.com>
2023-09-11 18:03:52 +0200

    Updated DataSource for streams

--- lib/data_sources/favorite_spells_local_data_source.dart
+++ lib/data_sources/favorite_spells_local_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:collection/collection.dart';
 import 'package:dnd_app/entities/spell_entity.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -9,20 +10,40 @@ const favoriteSpellsKey = 'fav-spells';
 
 class FavoriteSpellsLocalDataSource {
   Future<void> addFavouriteSpell({required SpellEntity spell}) async {
-    var spellSet = await getFavoriteSpells();
+    var spellSet = await _getFavoriteSpells();
     spellSet.add(spell);
 
     await _updateFavoriteSpells(updatedSpells: spellSet);
   }
 
   Future<void> deleteFavoriteSpell({required SpellEntity spell}) async {
-    var spellSet = await getFavoriteSpells();
+    var spellSet = await _getFavoriteSpells();
     spellSet.removeWhere((element) => element.index == spell.index);
 
     await _updateFavoriteSpells(updatedSpells: spellSet);
   }
 
-  Future<Set<SpellEntity>> getFavoriteSpells() async {
+  Future<Stream<Set<SpellEntity>>> getAllFavoriteSpellsStream() async {
+    final box = await Hive.openBox(favoriteSpellsBox);
+    return box.watch(key: favoriteSpellsKey).asyncMap(
+          (event) => _decodeSpells(event.value),
+        );
+  }
+
+  Future<Stream<SpellEntity?>> getSingleFavouriteSpellStream(
+      {required SpellEntity spell}) async {
+    final box = await Hive.openBox(favoriteSpellsBox);
+    final allSpellStream = box.watch(key: favoriteSpellsKey).asyncMap(
+          (event) => _decodeSpells(event.value),
+        );
+    final stream = allSpellStream.asyncMap((setOfSpells) {
+      return setOfSpells
+          .firstWhereOrNull((element) => element.index == spell.index);
+    });
+    return stream;
+  }
+
+  Future<Set<SpellEntity>> _getFavoriteSpells() async {
     final box = await Hive.openBox(favoriteSpellsBox);
     final spellsJson = await box.get(favoriteSpellsKey);
     box.close();
@@ -32,13 +53,6 @@ class FavoriteSpellsLocalDataSource {
     }
     final spells = _decodeSpells(spellsJson);
     return spells;
-  }
-
-  Future<Stream<Set<SpellEntity>>> getAllFavoriteSpellsStream() async {
-    final box = await Hive.openBox(favoriteSpellsBox);
-    return box.watch(key: favoriteSpellsKey).asyncMap(
-          (event) => _decodeSpells(event.value),
-        );
   }
 
   Future<void> _updateFavoriteSpells(
