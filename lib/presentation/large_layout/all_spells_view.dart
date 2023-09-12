3673d7736c3ae58d4ed1b68eb865269e09017b58
Goolski <andrzej.golski1@gmail.com>
2023-09-12 15:54:59 +0200

    Refactor and displaying error message

--- lib/presentation/large_layout/all_spells_view.dart
+++ lib/presentation/large_layout/all_spells_view.dart
@@ -1,46 +1,77 @@
+import 'dart:async';
+
 import 'package:dnd_app/presentation/common_widgets/spell_list_tile_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../data_sources/spells_data_source.dart';
 import '../../di/di.dart';
 import '../../entities/spell_entity.dart';
 
-class AllSpellsView extends StatefulWidget {
+class AllSpellsView extends StatelessWidget {
   const AllSpellsView({super.key});
-
-  @override
-  State<AllSpellsView> createState() => _AllSpellsViewState();
-}
-
-class _AllSpellsViewState extends State<AllSpellsView> {
-  late final Future<List<SpellEntity>> spells;
-
-  @override
-  void initState() {
-    spells = Injector.resolve<SpellsDataSource>().getSpells();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: spells,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) => SpellListTileWidget(
-                spell: snapshot.data![index],
-              ),
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+      body: ChangeNotifierProvider(
+        create: (context) => AllSpellsViewController(
+          (message) => showSnackbarMessage(message, context),
+        ),
+        child: Consumer<AllSpellsViewController>(
+          builder: (context, controller, child) {
+            switch (controller.spells.isNotEmpty) {
+              case true:
+                return ListView.builder(
+                  itemCount: controller.spells.length,
+                  itemBuilder: (context, index) => SpellListTileWidget(
+                    spell: controller.spells[index],
+                  ),
+                );
+              case false:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+            }
+          },
+        ),
       ),
     );
+  }
+
+  void showSnackbarMessage(String message, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+}
+
+class AllSpellsViewController extends ChangeNotifier {
+  AllSpellsViewController(this.showErrorMessage) {
+    _init();
+  }
+
+  Function(String) showErrorMessage;
+
+  List<SpellEntity> _spells = [];
+  List<SpellEntity> get spells => _spells;
+
+  final SpellsDataSource _spellsDataSource =
+      Injector.resolve<SpellsDataSource>();
+
+  Future<void> _init() async {
+    try {
+      await getSpells();
+    } catch (_) {
+      showErrorMessage('Error');
+    }
+  }
+
+  Future<void> getSpells() async {
+    final newSpells = await _spellsDataSource.getSpells();
+    _spells = newSpells;
+    notifyListeners();
   }
 }
