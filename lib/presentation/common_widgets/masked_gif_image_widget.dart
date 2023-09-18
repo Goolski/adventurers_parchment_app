30a2cde1338e08d593105212448e074187d4e7ce
Goolski <andrzej.golski1@gmail.com>
2023-09-18 18:01:07 +0200

    IT WORKS!

--- lib/presentation/common_widgets/masked_gif_image_widget.dart
+++ lib/presentation/common_widgets/masked_gif_image_widget.dart
@@ -20,48 +20,58 @@ class MaskedGifImageWidget extends StatefulWidget {
   State<MaskedGifImageWidget> createState() => _MaskedImageWidgetState();
 }
 
-class _MaskedImageWidgetState extends State<MaskedGifImageWidget> {
-  ImageInfo? currentImage = null;
+class _MaskedImageWidgetState extends State<MaskedGifImageWidget>
+    with SingleTickerProviderStateMixin {
+  late Future<List<ImageInfo>> images;
+  late AnimationController animController;
+  late Animation<int> animation;
 
   @override
   void initState() {
     super.initState();
-    getGifImages().then(
-      (value) => GifStream(
-        fps: 30,
-        gifImages: value,
-      ).stream.listen(
-        (event) {
-          setState(
-            () {
-              currentImage = event;
-            },
-          );
-        },
-      ),
+
+    animController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
     );
+
+    images = getGifImages().then((value) {
+      animation =
+          IntTween(begin: 0, end: value.length - 1).animate(animController)
+            ..addListener(() {
+              setState(() {});
+            });
+      animController.forward();
+      return value;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if (currentImage != null) {
-      return ShaderMask(
-        blendMode: widget.blendMode,
-        shaderCallback: (bounds) => ImageShader(
-          currentImage!.image,
-          TileMode.repeated,
-          TileMode.repeated,
-          Matrix4.identity().storage,
-        ),
-        child: widget.child,
-      );
-    } else {
-      return const SizedBox.shrink();
-    }
+    return FutureBuilder<List<ImageInfo>>(
+      future: images,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ShaderMask(
+            blendMode: widget.blendMode,
+            shaderCallback: (bounds) => ImageShader(
+              snapshot.data![animation.value].image,
+              TileMode.repeated,
+              TileMode.repeated,
+              Matrix4.identity().storage,
+            ),
+            child: widget.child,
+          );
+        } else {
+          return SizedBox.expand();
+        }
+      },
+    );
   }
 
   @override
   void dispose() {
+    animController.dispose();
     super.dispose();
   }
 
@@ -74,37 +84,11 @@ class _MaskedImageWidgetState extends State<MaskedGifImageWidget> {
     ui.Codec codec =
         await PaintingBinding.instance!.instantiateImageCodec(bytes);
     List<ImageInfo> infos = [];
-    Duration duration = Duration();
 
     for (int i = 0; i < codec.frameCount; i++) {
       ui.FrameInfo frameInfo = await codec.getNextFrame();
       infos.add(ImageInfo(image: frameInfo.image));
-      duration += frameInfo.duration;
     }
     return infos;
   }
-}
-
-class GifStream {
-  final List<ImageInfo> gifImages;
-  final int fps;
-  final StreamController<ImageInfo> _controller = StreamController();
-
-  GifStream({
-    required this.gifImages,
-    required this.fps,
-  }) {
-    init();
-  }
-
-  void init() {
-    for (var i = 0; i < 30; i++) {
-      Future.delayed(
-        Duration(milliseconds: 100),
-        () => _controller.add(gifImages[i]),
-      );
-    }
-  }
-
-  Stream<ImageInfo> get stream => _controller.stream;
 }
--- lib/presentation/mobile_layout/gif_test_view.dart
+++ lib/presentation/mobile_layout/gif_test_view.dart
@@ -1,35 +1,31 @@
 import 'package:dnd_app/presentation/common_widgets/masked_gif_image_widget.dart';
-import 'package:dnd_app/presentation/mobile_layout/main_scaffold.dart';
 import 'package:flutter/material.dart';
+
+final text = '''
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pharetra leo eros, et mattis erat consequat sed. Quisque vitae ligula a magna ornare porttitor. Maecenas molestie porttitor dolor. Nulla ornare, elit nec convallis euismod, urna leo interdum sem, ac rutrum tellus tortor nec leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris vehicula porta est. Nulla luctus nunc ac justo tincidunt, ornare eleifend tortor cursus. Nulla dapibus eros porta tortor consequat, at feugiat justo imperdiet. Phasellus consequat vehicula risus vitae sagittis. Vivamus a magna quis orci maximus luctus. Fusce sit amet pellentesque augue. Vestibulum lorem dui, consectetur vel varius at, tempor non sapien. Vivamus lacinia eleifend purus sit amet rhoncus.
+
+Suspendisse potenti. In cursus, justo tempus congue accumsan, quam mauris mollis risus, vitae lobortis erat purus vitae dolor. Sed finibus, purus vel consequat fermentum, magna turpis varius purus, ut blandit massa felis nec lorem. Morbi cursus purus a leo luctus dictum. Nam mattis dui urna, eget cursus magna commodo sit amet. Ut nulla urna, vehicula vel ipsum eu, convallis consequat nunc. Donec ornare interdum pellentesque. Aenean sed est laoreet, lacinia nunc viverra, accumsan magna.
+
+Fusce vel elit eu ante vehicula aliquam. Proin sit amet laoreet nisi. Sed faucibus euismod nulla at aliquam. Pellentesque aliquet luctus arcu, sit amet consequat nulla feugiat accumsan. In tristique augue nec sapien dapibus rutrum. Maecenas non bibendum justo, at auctor quam. Vivamus maximus ipsum ac nibh facilisis, a sodales ex tincidunt. Maecenas vel arcu venenatis, dictum quam ut, pellentesque urna. Ut consectetur convallis nisi, sit amet efficitur ex ullamcorper non. Nulla augue lorem, tincidunt id felis eget, dapibus convallis sapien. Cras efficitur risus vel porta scelerisque. Sed quam lorem, laoreet vitae ullamcorper sed, sollicitudin in nulla.
+
+Cras dictum venenatis est, eu dapibus nunc pellentesque quis. Sed vel magna ligula. Sed convallis eleifend nunc in euismod. Pellentesque venenatis laoreet orci, gravida viverra lacus auctor et. Suspendisse purus nunc, interdum eu laoreet sed, lobortis nec massa. In sapien nisi, lobortis at magna quis, porttitor tincidunt mi. Donec pellentesque malesuada nulla id ornare. Nulla finibus sem urna, nec rhoncus enim tempus in. Quisque ultricies nunc nec lectus congue porttitor. Aenean vel finibus nisi.
+
+Curabitur volutpat lorem eu ultricies suscipit. Nulla consequat, sem sit amet porta viverra, orci nisi elementum risus, ut consequat justo turpis a eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque cursus odio in rutrum commodo. In eget tristique tortor, id condimentum sem. Nullam posuere eu tellus in dictum. Phasellus in euismod libero, eget interdum sem. Morbi at commodo lectus. ''';
 
 class GifTestView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Blend Child Widget with Custom Painter'),
-        ),
-        body: SingleChildScrollView(
-          child: Stack(
-            children: [
-              MainBackgroundWidget(),
-              MaskedGifImageWidget(
-                image: AssetImage('assets/noise.gif'),
-                child: Text('''
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus in neque varius, lacinia neque ac, hendrerit sapien. Morbi rutrum pellentesque sapien quis pulvinar. Phasellus ut ornare augue. In nisl justo, congue nec augue nec, efficitur euismod mauris. Ut lacus ligula, fermentum molestie mi vitae, iaculis vestibulum lectus. Praesent aliquam imperdiet lorem eget tempus. In hac habitasse platea dictumst. Vivamus eu molestie nulla, eu dictum velit. Ut molestie enim felis, egestas eleifend velit malesuada dignissim. Sed vel efficitur diam, vitae bibendum purus. Vivamus feugiat id lorem vel porttitor. Duis malesuada pellentesque lacinia.
-
-Nulla mattis sem id sem laoreet faucibus sed et enim. Maecenas in augue vitae lorem cursus convallis. Suspendisse ut suscipit purus, in fermentum arcu. Quisque pulvinar sem a felis congue rhoncus id ut lorem. Cras est diam, lacinia sit amet erat sit amet, sagittis volutpat nibh. Suspendisse potenti. Curabitur pellentesque commodo volutpat. Etiam id porttitor lacus, nec lacinia mi. In hac habitasse platea dictumst. In scelerisque purus quis arcu dictum, quis ultrices diam viverra. Curabitur turpis purus, euismod tincidunt mattis non, porttitor id odio. Nullam neque augue, consectetur non urna vel, aliquet interdum enim. Duis vulputate, ex eu ultrices tincidunt, sapien ligula tincidunt metus, vel scelerisque dolor purus ut nunc. Aliquam odio dolor, imperdiet sit amet risus eget, condimentum molestie urna. Nulla sem dui, imperdiet in sagittis eget, malesuada vel leo.
-
-Nunc vestibulum pharetra molestie. In vehicula orci est, a porta leo condimentum fermentum. Phasellus sollicitudin, nisl nec condimentum facilisis, ligula risus faucibus sem, ac fermentum elit purus ac nunc. Etiam id sapien ex. Integer eu massa eu dolor aliquet elementum. Curabitur ac imperdiet leo, et sagittis lorem. Nulla fringilla vestibulum auctor. Cras condimentum metus tempus lacus varius mattis. Donec nulla mauris, tristique fermentum turpis vel, fermentum ornare urna. Nullam orci nunc, pellentesque vel dignissim ac, ullamcorper id turpis. Curabitur lobortis pellentesque lorem, vitae commodo erat molestie vehicula. Mauris vitae consectetur ex. Curabitur id rutrum tellus, vitae condimentum sapien. Duis tempus feugiat risus volutpat tempus. Curabitur enim ipsum, tincidunt a accumsan at, faucibus eu enim. Sed vehicula aliquam erat, elementum dignissim justo fermentum sit amet.
-
-Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer vitae fermentum nisl, at feugiat lorem. Curabitur et mattis erat. Nulla facilisi. Pellentesque egestas lacus odio, ac tincidunt erat bibendum quis. Suspendisse maximus orci magna. Vestibulum quis faucibus lacus. Proin venenatis euismod nisl, non tincidunt purus tristique vel. Suspendisse scelerisque lorem mauris, eleifend volutpat ante lacinia nec.
-
-Suspendisse mollis dictum ante, id tempor dolor varius et. Sed vitae eleifend ex. Curabitur mattis feugiat libero id pretium. Cras vitae nisi eget mauris viverra consectetur sed ut nulla. Phasellus eleifend nulla eget semper consectetur. Integer quis libero tellus. Morbi scelerisque enim ipsum, sit amet tincidunt metus sodales et. Fusce vestibulum, eros quis vestibulum maximus, nisi augue ullamcorper purus, sit amet dictum sapien felis et risus. Aenean gravida hendrerit nisl et tincidunt. Suspendisse varius rhoncus arcu ac mollis. Quisque auctor, magna eu accumsan tincidunt, orci sem laoreet turpis, congue vulputate ipsum ex molestie turpis. '''),
-                blendMode: BlendMode.dstIn,
-              ),
-            ],
+    return Scaffold(
+      body: Container(
+        color: Colors.greenAccent,
+        padding: EdgeInsets.all(20),
+        // child: Text('Hello World'),
+        child: SingleChildScrollView(
+          child: MaskedGifImageWidget(
+            image: AssetImage('assets/noise.gif'),
+            blendMode: BlendMode.dstIn,
+            child: Text(text),
           ),
         ),
       ),
