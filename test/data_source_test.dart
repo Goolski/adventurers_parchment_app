f57d89e10675ef6e69f5ac684eeb62c66062c36e
Goolski <andrzej.golski1@gmail.com>
2023-11-07 10:12:01 +0100

    Updated tests

--- test/data_source_test.dart
+++ test/data_source_test.dart
@@ -2,38 +2,34 @@ import 'dart:async';
 
 import 'package:adventurers_parchment/data_sources/characters_local_data_source.dart';
 import 'package:adventurers_parchment/data_sources/data_source_interface.dart';
+import 'package:adventurers_parchment/data_sources/database.dart';
 import 'package:adventurers_parchment/entities/character_entity.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final ICrudDataSource dataSource = CharactersLocalDataSource();
-  final character = CharacterEntity.empty(characterName: 'John');
-  test(
-    'Adding item to database should result in creating the object in database',
-    () async {
-      final StreamSubscription strSub = dataSource.getAll().listen((event) {
-        expect(event.contains(character), true);
-      });
-      await dataSource.add(item: character);
-      strSub.cancel();
+  final john = CharacterEntity.empty(characterName: 'John');
+  final andrew = CharacterEntity.empty(characterName: 'Andrew');
+  final jacob = CharacterEntity.empty(characterName: 'Jacob');
+
+  final initialCharacters = [john, andrew, jacob];
+
+  late ICrudDataSource<CharacterEntity> dataSource;
+
+  setUp(
+    () {
+      dataSource = CharactersLocalDataSource(
+        Database<CharacterEntity>(
+          initList: initialCharacters,
+        ),
+      );
     },
   );
 
-  test('Deleting an item should result in deleting it from database', () async {
-    dataSource.add(item: character);
-    final StreamSubscription strSub = dataSource.getAll().listen((event) {
-      expect(event.contains(character), false);
-    });
-    await dataSource.delete(item: character);
-    strSub.cancel();
-  });
-
   test('You should be able to get item by Id when subscribing to stream',
       () async {
-    dataSource.add(item: character);
     final StreamSubscription strSub =
-        dataSource.get(id: character.id).listen((event) {
-      expect(event, character);
+        dataSource.get(id: john.id).listen((event) {
+      expect(event, john);
     });
     strSub.cancel();
   });
@@ -41,7 +37,34 @@ void main() {
   test(
       'You should be able to get all current items only by subscribing to stream',
       () async {
-    final character2 = CharacterEntity.empty(characterName: 'Andrew');
-    await dataSource.add(item: character2);
+    final StreamSubscription strSub = dataSource.getAll().listen((event) {
+      expect(event, initialCharacters);
+    });
+    strSub.cancel();
+  });
+
+  test(
+    'Adding item to database should result in creating the object in database',
+    () async {
+      final newCharacter = CharacterEntity.empty(characterName: 'new');
+
+      final charactersBeforeAdding = await dataSource.getAll().first;
+      expect(charactersBeforeAdding.contains(newCharacter), false);
+
+      dataSource.add(item: newCharacter);
+
+      final charactersAfterAdding = await dataSource.getAll().first;
+      expect(charactersAfterAdding.contains(newCharacter), true);
+    },
+  );
+
+  test('Deleting an item should result in deleting it from database', () async {
+    final charactersBeforeDeleting = await dataSource.getAll().first;
+    expect(charactersBeforeDeleting.contains(john), true);
+
+    dataSource.delete(item: john);
+
+    final charactersAfterDeleting = await dataSource.getAll().first;
+    expect(charactersAfterDeleting.contains(john), false);
   });
 }
