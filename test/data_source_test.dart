98b01bcaaf304be9cf837baf4e0aaec7b2764c37
Goolski <andrzej.golski1@gmail.com>
2023-11-09 13:34:30 +0100

    Created tests for duplicating the same item or deleting non-existing item

--- test/data_source_test.dart
+++ test/data_source_test.dart
@@ -4,6 +4,7 @@ import 'package:adventurers_parchment/data_sources/characters_local_data_source.
 import 'package:adventurers_parchment/data_sources/data_source_interface.dart';
 import 'package:adventurers_parchment/data_sources/database.dart';
 import 'package:adventurers_parchment/entities/character_entity.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -19,29 +20,31 @@ void main() {
     () {
       dataSource = CharactersLocalDataSource(
         Database<CharacterEntity>(
-          initList: initialCharacters,
+          initList: List.from(initialCharacters),
         ),
       );
     },
   );
-
-  test('You should be able to get item by Id when subscribing to stream',
-      () async {
-    final StreamSubscription strSub =
-        dataSource.get(id: john.id).listen((event) {
-      expect(event, john);
-    });
-    strSub.cancel();
-  });
+  test(
+    'You should be able to get item by Id when subscribing to stream',
+    () async {
+      final StreamSubscription strSub =
+          dataSource.get(id: john.id).listen((event) {
+        expect(event, john);
+      });
+      strSub.cancel();
+    },
+  );
 
   test(
-      'You should be able to get all current items only by subscribing to stream',
-      () async {
-    final StreamSubscription strSub = dataSource.getAll().listen((event) {
-      expect(event, initialCharacters);
-    });
-    strSub.cancel();
-  });
+    'You should be able to get all current items only by subscribing to stream',
+    () async {
+      final StreamSubscription strSub = dataSource.getAll().listen((event) {
+        expect(event, initialCharacters);
+      });
+      strSub.cancel();
+    },
+  );
 
   test(
     """You should be able
@@ -71,28 +74,110 @@ void main() {
     },
   );
 
-  test('Deleting an item should result in deleting it from database', () async {
-    final charactersBeforeDeleting = await dataSource.getAll().first;
-    expect(charactersBeforeDeleting.contains(john), true);
+  test(
+    'Deleting an item should result in deleting it from database',
+    () async {
+      final charactersBeforeDeleting = await dataSource.getAll().first;
+      expect(charactersBeforeDeleting.contains(john), true);
 
-    dataSource.delete(item: john);
+      dataSource.delete(item: john);
 
-    final charactersAfterDeleting = await dataSource.getAll().first;
-    expect(charactersAfterDeleting.contains(john), false);
-  });
+      final charactersAfterDeleting = await dataSource.getAll().first;
+      expect(charactersAfterDeleting.contains(john), false);
+    },
+  );
 
-  (test(
+  test(
     'Updating an item should not delete it temporarily',
+    () async {
+      final updatedJohn = john.copyWith(name: 'updatedJohn');
+
+      List<Iterable<CharacterEntity>> allEvents = [];
+
+      final strSub = dataSource.getAll().listen(
+        (event) {
+          allEvents.add(event);
+        },
+      );
+
+      await dataSource.update(item: updatedJohn);
+
+      strSub.cancel();
+
+      expect(
+        allEvents.any(
+          (iterable) => listEquals(iterable.toList(), [andrew, jacob]),
+        ),
+        false,
+      );
+    },
+  );
+
+  test(
+    'Updating an item should update Item',
     () async {
       final updatedJohn = john.copyWith(name: 'updatedJohn');
       final updatedCharacters = [updatedJohn, andrew, jacob];
 
       expectLater(
-        dataSource.getAll(),
-        emitsInOrder([equals(initialCharacters), equals(updatedCharacters)]),
-      );
+          dataSource.getAll(),
+          emitsInOrder([
+            equals(initialCharacters),
+            equals(updatedCharacters),
+          ]));
+
+      await Future.delayed(Duration.zero);
 
       await dataSource.update(item: updatedJohn);
     },
-  ));
+  );
+
+  test(
+    'Adding duplicate should result in no changes',
+    () async {
+      await dataSource.add(item: john);
+
+      final characters = await dataSource.getAll().first;
+
+      expect(listEquals(characters.toList(), initialCharacters), true);
+    },
+  );
+
+  test(
+    'Adding duplicate should throw Exception',
+    () async {
+      expectLater(
+        dataSource.add(item: john),
+        throwsA(
+          isA<ItemAlreadyExistsException>(),
+        ),
+      );
+    },
+  );
+
+  test(
+    'Deleting non existing item should throw Exception',
+    () async {
+      final newCharacter = CharacterEntity.empty(characterName: 'newCharacter');
+      expectLater(
+        dataSource.delete(item: newCharacter),
+        throwsA(
+          isA<ItemDoesntExistException>(),
+        ),
+      );
+    },
+  );
+
+  test(
+    'Updating non existing item should throw Exception',
+    () async {
+      final newCharacter = CharacterEntity.empty(characterName: 'newCharacter');
+      expectLater(
+        dataSource.update(item: newCharacter),
+        throwsA(
+          isA<ItemDoesntExistException>(),
+        ),
+      );
+    },
+  );
 }
